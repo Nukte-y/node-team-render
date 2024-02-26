fc37b9f80afbff305a9d0be045c3f28d4fd31f9c
nukte-y <nukteu@gmail.com>
2024-02-26 15:16:31 +0000

    updated README and added styles

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # node-team-render
 Generate an HTML web page displaying based on input from the command line.
+
+## Table of Contents
+- [Description](#description)
+- [Usage](#usage)
+- [Screenshots](#screenshots)
+- [Tests](#tests)
+- [GitHub](#github)
+- [License](#license)
+
+## Description
+This project is a command-line application that generates professional README files for your projects. It simplifies the process by prompting the user for information about the project and then dynamically creates a README.md file based on the responses.
+
+## Usage
+To use the application, follow these steps:
+
+1. Run the command `node index.js`.
+2. Follow the prompts to input information about your team members.
+3. Once all prompts are completed, the HTML file will be generated in the current directory.
+
+## Screenshots
+![Screenshot](./assets/html.PNG)
+
+## Tests
+
+To run the tests, run the command: npm run test
+
+![Screenshot](./assets/tests.PNG)
+
+## GitHub
+Github: **[Nukte-Y](https://github.com/Nukte-y/node-team-render)**
+
+## License
+This project is licensed under the terms of the [MIT License](LICENSE).
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
--- src/page-template.js
+++ src/page-template.js
@@ -4,12 +4,12 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
+        <div class="card employee-card" ; style = "margin: 5px;">
+        <div class="card-header" style="background-color: #ff5722 ; color: white;">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body" style="background-color: #f8f9fa">
             <ul class="list-group">
                 <li class="list-group-item">ID: ${manager.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
@@ -23,12 +23,12 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card" style = "margin: 5px;">
+    <div class="card-header" style="background-color: #5cbde5 ">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="background-color: #f8f9fa">
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
@@ -42,12 +42,12 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card" style = "margin: 5px;">
+    <div class="card-header" style="background-color: #e83e8cbd ; color: white">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="background-color: #f8f9fa">
         <ul class="list-group">
             <li class="list-group-item">ID: ${intern.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
@@ -100,7 +100,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading" style="background-color: #FFEB3B">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
